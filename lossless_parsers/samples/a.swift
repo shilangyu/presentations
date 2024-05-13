b59f71e718ea1636108977635efb913671509dea
shilangyu <xmarcinmarcin@gmail.com>
2024-05-13 13:39:59 +0200

    Initial work

---
+++ lossless_parsers/samples/a.swift
@@ -0,0 +1,8 @@
+func fun() -> Int {
+    1
+}
+
+let t: String = fun(12 + )
+
+
+let a: Int = "asd"
---
+++ lossless_parsers/samples/a.ts
@@ -0,0 +1,5 @@
+function func() { return 1; }
+
+const t: string = func(12 + )
+
+const a: number = "asd"
